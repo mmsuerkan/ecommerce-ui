521cb26983e8597871023738b32a5c31c2c53326
Mustafa Mert SÜERKAN <msuerkan@partner.innova.com.tr>
2022-06-12 20:22:31 +0300

    Adding A Category

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,37 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import AddCategory from "../views/Category/AddCategory";
+import Category from "../views/Category/Category";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/admin/category/add",
+    name: "AddCategory",
+    component: AddCategory,
+  },
+  {
+    path: "/admin/category",
+    name: "AdminCategory",
+    component: Category,
+  },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
