88196e7d1921d53d732b5e05df100aee1e002f8a
Mustafa Mert SÜERKAN <msuerkan@partner.innova.com.tr>
2022-06-12 22:00:56 +0300

    Build The Product Detail Page

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import EditCategory from "../views/Category/EditCategory";
 import AddProduct from "../views/Product/AddProduct";
 import Product from "../views/Product/Product";
 import EditProduct from "../views/Product/EditProduct";
+import ShowDetails from "../views/Product/ShowDetails";
 
 const routes = [
   {
@@ -42,6 +43,11 @@ const routes = [
     path: "/admin/product/:id",
     name: "EditProduct",
     component: EditProduct,
+  },
+  {
+    path : '/product/show/:id',
+    name : 'ShowDetails',
+    component: ShowDetails
   }
 ];
 
